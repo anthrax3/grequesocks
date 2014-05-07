60b1950422244e4bf685d87958aa7f74a3c912d5
Tomer Zait <tomerz@ironsrc.com>
2014-05-07 13:00:39 +0300

    Fixed Setup Error

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 __author__ = 'RealGame (Tomer Zait)'
 
-import grequesocks
 from distutils.core import setup
 
 
 setup(
     name='grequesocks',
-    version=grequesocks.__version__,
+    version='1.2',
     description='Grequests Monkey Patch To Use Requesocks',
     author='RealGame (Tomer Zait)',
     author_email='realgam3@gmail.com',
